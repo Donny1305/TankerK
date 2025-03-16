8616fbc7e81a1f75e63ebe99bebaa437e69f9f8d
Donny1305 <62388504+Donny1305@users.noreply.github.com>
2025-03-16 17:34:25 +0100

    PLEASE WORK THSI TIME

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ try:
     from kivy.app import App
     import requests
     import random
-
+    import ssl
 except BaseException as e:
     open("/error_log.txt", "w").write(e)
 
 class MapViewTanker(FloatLayout):
     def __init__(self, **kwargs):
+        ssl._create_default_https_context = ssl._create_stdlib_context
         super().__init__(**kwargs)
         lat = 48.47728212579956
         lon = 7.955887812049504
@@ -20,14 +21,13 @@ class MapViewTanker(FloatLayout):
         assert isinstance(self.map, MapView)
         self.map.center_on(lat, lon)
         self.map.zoom = 5
-        """
         rad = 5
         type = 'e5'
         key = '1e89035b-ed46-fdc3-4baf-feff2614dc10'
         url = 'https://creativecommons.tankerkoenig.de/json/list.php?lat=' + str(lat) + '&lng=' + str(lon) + '&rad=' + str(rad) + '&sort=dist&type=' + type + '&apikey=' + key
         data = requests.get(url)
-        data = data.json()
-        """
+        print(data)
+
     
     def randomZoom(self):
         lat = random.randint(-90, 90)
