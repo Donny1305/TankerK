1eecd57a28d70b9fe5afae1b18a82e692cbd4fe3
Donny1305 <62388504+Donny1305@users.noreply.github.com>
2025-03-17 10:23:10 +0100

    code style changes

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
-try:
-    from kivymd.app import MDApp
-    from kivy.uix.floatlayout import FloatLayout
-    from kivy.uix.label import Label
-    from kivy.lang import Builder
-    from kivy_garden.mapview import MapView, MapMarkerPopup
-    import requests
-    import ssl
-except BaseException as e:
-    open("/error_log.txt", "w").write(e)
+from kivymd.app import MDApp
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.lang import Builder
+from kivy_garden.mapview import MapView, MapMarkerPopup
+import requests
+import ssl
 
+class ApiCaller():
+    def __init__(self):
+        self.__rad = 5
+        self.__type = 'e5'
+        self.__key = '1e89035b-ed46-fdc3-4baf-feff2614dc10'
+        self.__url = 'https://creativecommons.tankerkoenig.de/json/list.php'
+
+    def getQueriedTankerData(self, lat, lon):
+        url = self.__url + "?lat=" + str(lat) + '&lng=' + str(lon) + '&rad=' + str(self.__rad) + '&sort=dist&type=' + self.__type + '&apikey=' + self.__key
+        data = requests.get(url)
+
+        return data.json()
+    
 class MapViewTanker(FloatLayout):
     def __init__(self, **kwargs):
         ssl._create_default_https_context = ssl._create_stdlib_context
         super().__init__(**kwargs)
+
         lat = 48.47728212579956
         lon = 7.955887812049504
+
         self.map = self.ids.tankerMap
-        assert isinstance(self.map, MapView)
         self.map.center_on(lat, lon)
         self.map.zoom = 20
-        rad = 5
-        type = 'e5'
-        key = '1e89035b-ed46-fdc3-4baf-feff2614dc10'
-        url = 'https://creativecommons.tankerkoenig.de/json/list.php?lat=' + str(lat) + '&lng=' + str(lon) + '&rad=' + str(rad) + '&sort=dist&type=' + type + '&apikey=' + key
-        data = requests.get(url)
-        data = data.json()
+        assert isinstance(self.map, MapView)
+
+        apiCaller = ApiCaller()
+        data = apiCaller.getQueriedTankerData(lat, lon)
 
         self.setLowestAndHighestPrice(data)
-
+        self.generateMarkersForData(data)
+    
+    def generateMarkersForData(self, data):
         for dataSet in data.get('stations'):
             stationLat = dataSet.get('lat')
             stationLon = dataSet.get('lng')
@@ -35,6 +46,7 @@ class MapViewTanker(FloatLayout):
             price = dataSet.get('price')
             street = dataSet.get('street') + " " + str(dataSet.get('houseNumber'))
             location = str(dataSet.get('postCode')) + " " + dataSet.get('place')
+
             address = street
             address += "\n" + location
             address += "\n" + title
@@ -50,12 +62,10 @@ class MapViewTanker(FloatLayout):
             label.outline_width = 4
             marker.popup_size = 100, 100
             marker.add_widget(label)
-            
+
             self.map.add_marker(marker)
 
     def getMarkerSourceForPrice(self, price):
-        print(self.lowestPrice * 1.02)
-        print(price)
         if (self.lowestPrice == price):
             return 'green32.png'
         
@@ -81,6 +91,7 @@ class TankerApp(MDApp):
     def build(self):
         self.icon = 'tankericon.png'
         Builder.load_file("map.kv")
+        
         return MapViewTanker()
     
 if __name__ == '__main__':
